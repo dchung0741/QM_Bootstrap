d254b9c8c5edb1581f329bf76b38a45f41106c42
Daniel <30490924+dchung0741@users.noreply.github.com>
2022-01-27 22:29:31 +0800

    Universal boostrap code

---
+++ Bootstrap.py
@@ -0,0 +1,50 @@
+from mpmath import mp, det
+from sympy.matrices import zeros
+
+
+class Bootstrap:
+
+    def __init__(self, E, Recurrence_Func, Initial_conition, Matrix_el_Fnc, Default_N = 30):
+        
+        self.E = E
+        self.N = Default_N
+        self.rec = Recurrence_Func
+        self.computed = Initial_conition
+        self.matrix_el_fnc = Matrix_el_Fnc
+
+        # Build Hankel matrix
+        self.H = mp.matrix(zeros(self.N + 1, self.N + 1))
+        for i in range(self.N + 1):
+
+            for j in range(i + 1):
+
+                try:
+                    self.H[i, j] = self.H[j, i] = self.matrix_el_fnc(i, j)
+                
+                except KeyError:
+
+                    if i + j not in self.computed:
+                        self.computed[i + j] = self.rec(i + j)
+                    
+                    if i + 1 not in self.computed:
+                        self.computed[i + 1] = self.rec(i + 1)
+                    
+                    if j + 1 not in self.computed:
+                        self.computed[j + 1] = self.rec(j + 1)
+                    
+                    self.H[i, j] = self.H[j, i] = self.matrix_el_fnc(i, j)
+    
+    
+    def is_Eigen(self, n, allowed = 1, not_allowed = 0):
+        
+        i = 1
+
+        while det((self.H)[0: i, 0: i]) >=0 and i <= n:
+            
+            i += 1
+        
+        if i - 1 == n:
+            return allowed
+        
+        else:
+            return not_allowed
